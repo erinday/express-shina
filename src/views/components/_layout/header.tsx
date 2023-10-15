49a36a178363e5f8fba9dc9a987370f023b82728
Erinday <erinday.team@gmail.com>
2023-10-15 16:49:41 +0300

    Add menu

--- src/views/components/_layout/header.tsx
+++ src/views/components/_layout/header.tsx
@@ -6,7 +6,7 @@ export default function Header (): JSX.Element {
   return (
     <div className="header">
       <div className="header__cont">
-        <NavLink to="/" end className={({ isActive }) => isActive ? 'header__logo' : 'header__logo header__logo_active' }>
+        <NavLink to="/" end className={({ isActive }) => isActive ? 'header__logo header__logo_active' : 'header__logo'}>
           <picture className="header__logo-picture">
             <img className="header__logo-image" src="/images/logo.png" alt=""/>
           </picture>
--- src/views/components/_layout/menu.tsx
+++ src/views/components/_layout/menu.tsx
@@ -1,9 +1,21 @@
 import { JSX } from 'react'
+import { NavLink } from 'react-router-dom'
+import '../../../styles/components/menu.sass'
 
 export default function Menu (): JSX.Element {
   return (
     <div className="menu">
-
+      <div className="menu__cont">
+        <nav className="menu__list">
+          <NavLink
+            className={({ isActive }) => isActive ? 'menu__item menu__item_active' : 'menu__item '}
+            to="/categories"
+            end
+          >
+            Категории
+          </NavLink>
+        </nav>
+      </div>
     </div>
   )
 }
